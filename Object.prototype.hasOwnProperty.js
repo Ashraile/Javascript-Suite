2612ae71ed5db2599495895bcb8e635f48a9eb89
Ashraile <89170464+Ashraile@users.noreply.github.com>
2025-05-16 04:18:48 -0500

    Create Object.prototype.hasOwnProperty.js

---
+++ Object.prototype.hasOwnProperty.js
@@ -0,0 +1,22 @@
+Object.prototype.hasOwnProperty || (function() {
+
+    function hasOwnProperty(prop) {
+
+        if (this === null || this === undefined) { // strict equality eliminates document.all false-positive
+            throw new TypeError('Object.prototype.hasOwnProperty called on' + this);
+        }
+        var O = Object(this), key = String(prop);
+      
+        var _proto = (function(){ try { return O.__proto__ || O.constructor.prototype } catch(e){} })() || {}; // Object.prototype
+
+        return key in O && (!(key in _proto) || O[key] !== _proto[key]);
+    }
+
+    try { 
+        Object.defineProperty( Object.prototype, 'hasOwnProperty', { 
+            configurable: true, writable: true, value: hasOwnProperty // enumerable: false
+        });
+    } 
+    catch (e) { Object.prototype.hasOwnProperty = hasOwnProperty; } // Object.defineProperty isn't supported. Use dot notation syntax.
+
+})();
